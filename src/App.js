c99e5bcdb836f0d947efe908234cc2ea5e9e870b
lkh144 <lkh2444@gmail.com>
2024-04-10 12:52:19 +0900

    secondlkh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React this is gunjjang
+          Learn React this is gunjjang second
         </a>
       </header>
     </div>
